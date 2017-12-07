cfee520a9503fb8d639f2b47cdc3979931ff4fb6
ZaviiNet <trancefx@live.co.uk>
2017-12-07 13:44:53 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,43 @@
+exports.handler = function( event, context ) {
+
+    var http = require( 'https' );
+
+    var url = 'http://store.steampowered.com/api/appdetails?appids=';
+
+    http.get( url, function( response ) {
+
+        var data = '';
+
+        response.on( 'data', function( x ) { data += x; } );
+
+        response.on( 'end', function() {
+
+            var json = JSON.parse( data );
+            var text = 'Here's what I found about ';
+
+
+
+        } );
+
+    } );
+
+};
+
+function output( text, context ) {
+
+    var response = {
+        outputSpeech: {
+            type: "PlainText",
+            text: text
+        },
+        card: {
+            type: "Simple",
+            title: "SteamGameInfo",
+            content: text
+        },
+        shouldEndSession: true
+    };
+
+    context.succeed( { response: response } );
+
+}
